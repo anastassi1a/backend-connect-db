575b19778ffa294a12d6a7370d3d160674be46a9
anastassi1a <akor5665@gmail.com>
2024-05-26 18:45:23 +0500

    domain

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,15 +1,14 @@
-const allowedCors = ["http://localhost3000", "http://localhost3001"];
+const allowedCors = ["https://frontend.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
-    const { origin } = req.headers;
+  const { origin } = req.headers;
 
-    if (allowedCors.includes(origin)) {
-        res.header("Access-Control-Allow-Methods", 
-        "GET,HEAD,PUT,PATCH,POST,DELETE");
-        res.header("Access-Control-Allow-Headers", 
-        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-    }
-    next();
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 
-module.exports = cors;
+module.exports = cors; 
